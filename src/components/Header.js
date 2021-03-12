9d999abf45e61bb168fcf418a65fb0ff37d015c2
Lecio <lecio.vilela@yahoo.com>
2021-03-12 19:31:45 -0300

    add new project

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../css/styles.css'
+
+const Header = () => {
+    return (
+        <div className='ui fixed menu'>
+            <div className='ui container center'>
+                <h2 className='bg-red-300'>Gerenciador de Contatos</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header
